59cf7587e802573c5255c26f54aabd54e7b8965a
Eelise0 <106895470+Eelise0@users.noreply.github.com>
2022-09-06 11:57:09 -0700

    Working on Week 6 project

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
-console.log(hasStringAtEnd('hello', 'llo'));  //true
-console.log(hasStringAtEnd('llo', 'hello'));  //true
-console.log(hasStringAtEnd('llod', 'hello')); //false
-console.log(hasStringAtEnd('ll', 'hello'));   //false
-console.log(hasStringAtEnd('llo','hellowd')); //false
+// console.log(hasStringAtEnd('hello', 'llo'));  //true
+// console.log(hasStringAtEnd('llo', 'hello'));  //true
+// console.log(hasStringAtEnd('llod', 'hello')); //false
+// console.log(hasStringAtEnd('ll', 'hello'));   //false
+// console.log(hasStringAtEnd('llo','hellowd')); //false
 
-function hasStringAtEnd(a, b) {
-    let shortest = '';
-    let longest = '';
-    if (a.length < b.length) {
-        shortest = a;
-        longest = b;
-    } else {
-        shortest = b;
-        longest = a;
+// function hasStringAtEnd(a, b) {
+//     let shortest = '';
+//     let longest = '';
+//     if (a.length < b.length) {
+//         shortest = a;
+//         longest = b;
+//     } else {
+//         shortest = b;
+//         longest = a;
+//     }
+
+//     const indexStart = longest.length - shortest.length;
+//     const endOfLongest = longest.substring(indexStart);
+//     return shortest === endOfLongest;
+// }
+
+function doSomething(x, y) {
+    if (typeof x != 'string') {
+        throw new Error('x must be a string');
     }
-
-    const indexStart = longest.length - shortest.length;
-    const endOfLongest = longest.substring(indexStart);
-    return shortest === endOfLongest;
+    return x + y;
 }

3f913d43da6dd936ac792ca8cc9afb1021eb7839
Eelise0 <106895470+Eelise0@users.noreply.github.com>
2022-09-06 17:47:19 -0700

    Achieved most of assignment requirements

--- WarCardGame.js
+++ WarCardGame.js
@@ -1,5 +1,36 @@
 
-/************************ SHUFFLING**********************/
+// class Player {
+//     constructor(name) {
+//         this.name = name;
+//         this.hand = hand;
+//         this.score = [];
+//     }
+// }
+
+// class Card {
+//     constructor(name) {
+//         this.name = name;
+//         this.value = value;
+//     }
+// }
+
+// class Deck {
+//     constructor() {
+//         this.playersDeck = playersDeck;
+//         this.shuffle = shuffle;
+//     }
+// }
+
+alert `----- Let's play War! ----
+There are two players and the player with 
+the highest value card at draw wins the point.
+Whoever has the most points at the end wins!
+The game will automatically begin when you click OK.
+You won't see much on the screen but rest 
+assured the game is happening! To see all the action, 
+check out the Console. Good Luck!`
+
+/************************ SHUFFLE **********************/
 
 function shuffle(deck) {
     let currentIndex = deck.length,  randomIndex;
@@ -20,7 +51,8 @@ function shuffle(deck) {
   }
   
   // Used like so
-  var mainDeck = ['A', 'A', 'A', 'A', 'K', 'K', 'K', 'K', 'Q', 'Q', 'Q', 'Q', 'J', 'J', 'J', 'J', 10, 10, 10, 10, 9, 9, 9, 9, 8, 8, 8, 8, 7, 7, 7, 7, 6, 6, 6, 6, 5, 5, 5, 5, 4, 4, 4, 4, 3, 3, 3, 3, 2, 2, 2, 2];
+  
+  var mainDeck = [14, 14, 14, 14, 13, 13, 13, 13, 12, 12, 12, 12, 11, 11, 11, 11, 10, 10, 10, 10, 9, 9, 9, 9, 8, 8, 8, 8, 7, 7, 7, 7, 6, 6, 6, 6, 5, 5, 5, 5, 4, 4, 4, 4, 3, 3, 3, 3, 2, 2, 2, 2];
   shuffle(mainDeck);
   console.log(mainDeck);
 
@@ -36,25 +68,108 @@ console.log('Player One Deck: ' + playerOneDeck);
 playerTwoDeck = mainDeck.slice(26, 52);
 console.log('Player Two Deck: ' + playerTwoDeck);
 
-/*************************************************/
 
-// while (playerOneDeck.length > 0 && playerTwoDeck.length > 0) {
-//     for (i = 0; i < playerOneDeck.length; i++) {
-//         cardDrawnOne = null;
-//         cardDrawnTwo = null;
-//         cardDrawnOne = cardDrawnOne + playerOneDeck[i];
-//         cardDrawnTwo = cardDrawnTwo + playerTwoDeck[i]; }
-//         if (cardDrawnOne > cardDrawnTwo) {
-//             playerOnePoints = [];
-//             playerOnePoints.push(1);
-//             alert `1 point for Player One!`;
-//         } else if (cardDrawnTwo > cardDrawnOne) {
-//             playerTwoPoints = [];
-//             playerTwoPoints.push(1);
-//             alert `1 point for Player Two!`;
-//         } else {
-//             alert `It's a draw! No points for either player!`;
-//         }
-//         }
-       
+/********************* COMPARING THE CARDS ********************/
+
+var playerOneIndex = 0;
+var playerTwoIndex = 0;
+let playerOnePoints = [];
+let playerTwoPoints = [];
+
+{for (let i = 0; i < playerOneDeck.length; i++) {
+
+    playerOneIndex = playerOneDeck[i];
+    console.log('1: ' + playerOneIndex);
+
+   playerTwoIndex = playerTwoDeck[i];
+   console.log('2: ' + playerTwoIndex);
+
+if (playerOneIndex > playerTwoIndex) {
+    playerOnePoints.push(1);
+} else if (playerTwoIndex > playerOneIndex) {
+    playerTwoPoints.push(1);
+} else {
+
+}
+
+console.log('Player 1 Points: ' + playerOnePoints);
+console.log('Player 2 Points: ' + playerTwoPoints);
+
+}}
+
+let totalForPlayerOne = 0;
+ for (let i = 0; i < playerOnePoints.length; i++) {
+    totalForPlayerOne += playerOnePoints[i];
+ }
+console.log('Total for Player One: ' + totalForPlayerOne);
+
+ let totalForPlayerTwo = 0;
+ for (let i = 0; i < playerTwoPoints.length; i++) {
+    totalForPlayerTwo += playerTwoPoints[i];
+ }
+console.log('Total for Player Two: ' + totalForPlayerTwo);
+
+if (totalForPlayerOne > totalForPlayerTwo) {
+    alert ('Wow! Great job, Player 1! You have won the game with ' 
+    + totalForPlayerOne + ' points! Player 2, you only had ' + totalForPlayerTwo + '. Better luck next time!');
+} else 
+    alert ('Wow! Great job, Player 2! You have won the game with ' 
+    + totalForPlayerTwo + ' points! Player 2, you only had ' + totalForPlayerOne + ' points. Better luck next time!');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// if (playerOneIndex[i] === 'King' && playerTwoIndex[i] <= 10)  {
+//     playerOnePoints.push(1);
+//     alert `1 point for Player One!`;
+
+// } else if (playerOneIndex[i] === 'Queen' && playerTwoIndex[i] <= 10) {
+//     playerOnePoints.push(1);
+//     alert `1 point for Player One!`;
+
+// } else if (playerOneIndex[i] === 'Ace' && playerTwoIndex[i] <= 10) {
+//     playerOnePoints.push(1);
+//     alert `1 point for Player One!`;
+
+// } else if (playerOneIndex[i] === 'Jack' && playerTwoIndex[i] <= 10) {
+//     playerOnePoints.push(1);
+//     alert `1 point for Player One!`;
+
+// } else if (playerTwoIndex[i] === 'King' && playerOneIndex[i] <= 10) {
+//     playerTwoPoints.push(1);
+//     alert `1 point for Player Two!`;
+
+// } else if (playerTwoIndex[i] === 'Queen' && playerOneIndex[i] <= 10) {
+//     playerTwoPoints.push(1);
+//     alert `1 point for Player Two!`;
+
+// } else if (playerTwoIndex[i] === 'Ace' && playerOneIndex[i] <= 10) {
+//     playerTwoPoints.push(1);
+//     alert `1 point for Player Two!`;
+
+// } else if (playerTwoIndex[i] === 'Jack' && playerOneIndex[i] <= 10) {
+//     playerTwoPoints.push(1);
+//     alert `1 point for Player Two!`;
+
+// } else if (playerOneIndex[i] > playerTwoIndex[i]) {
+//     playerOnePoints.push(1);
+//     alert `1 point for Player One!`;
+
+// } else if (playerTwoIndex[i] > playerOneIndex[i]) {
+//     playerTwoPoints.push(1);
+//     alert `1 point for Player Two!`;
+
+// } else
+//     alert `No points for anyone!`
+
 
